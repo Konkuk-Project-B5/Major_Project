ffbc74fa4205eed3b84adb73c54eac0aa31c61c5
82104 <82104@Airmax>
2023-09-14 13:59:06 +0900

    github 다른컴퓨터 연동

--- TimeTable/src/main/TablePrint.java
+++ TimeTable/src/main/TablePrint.java
@@ -3,5 +3,7 @@ package main;
 public class TablePrint {
 	public static void main(String []args) {
 		System.out.println("Table print branch 연동");
+		System.out.println("Table print branch 연동2");
+
 	}
 }

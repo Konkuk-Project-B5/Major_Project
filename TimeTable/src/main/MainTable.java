343e72ed0520d62b564cfa571ccac5bcd318f7ae
parkSJ <parksu987@gmail.com>
2023-09-14 23:56:32 +0900

    test

--- TimeTable/src/main/MainTable.java
+++ TimeTable/src/main/MainTable.java
@@ -2,7 +2,7 @@ package main;
 
 public class MainTable {
 	public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("박수진");
         
 
     }

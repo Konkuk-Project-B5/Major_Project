5d8b96f5f925e10a51a0b69b6254dee5a32693de
82104 <82104@Airmax>
2023-09-14 14:00:11 +0900

    ytty

--- TimeTable/src/main/TablePrint.java
+++ TimeTable/src/main/TablePrint.java
@@ -4,6 +4,7 @@ public class TablePrint {
 	public static void main(String []args) {
 		System.out.println("Table print branch 연동");
 		System.out.println("Table print branch 연동2");
+		System.out.println("Table print branch 연동2");
 
 	}
 }

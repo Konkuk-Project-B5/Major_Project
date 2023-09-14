6d14270a6a07a7592e7491678c6a0e48ee349754
82104 <82104@Airmax>
2023-09-14 16:19:14 +0900

    cho

--- TimeTable/src/main/TablePrint.java
+++ TimeTable/src/main/TablePrint.java
@@ -2,9 +2,7 @@ package main;
 
 public class TablePrint {
 	public static void main(String []args) {
-		System.out.println("Table print branch 연동");
-		System.out.println("Table print branch 연동2");
-		sdfsadad
+		System.out.println("조성현");
 
 	}
 }

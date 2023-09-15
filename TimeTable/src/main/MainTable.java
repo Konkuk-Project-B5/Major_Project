cc1616b3e389c0dc8127dde892086ecac6ab9f73
meo-young <meoyoung000@naver.com>
2023-09-15 16:53:20 +0900

    maintable -> tableadd로 옮김

--- TimeTable/src/main/MainTable.java
+++ TimeTable/src/main/MainTable.java
@@ -2,14 +2,6 @@ package main;
 
 public class MainTable {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
-
-=======
-        System.out.println("박수진");
         
->>>>>>> 884896a9b0d3a5ae7b5df6b3e73a6937a687da3e
-
     }
 }
--- TimeTable/src/main/TableAdd.java
+++ TimeTable/src/main/TableAdd.java
@@ -1,5 +1,8 @@
 package main;
 
 public class TableAdd {
+    public static void main(String []args) {
+		System.out.println("박수진");
 
+	}
 }

9f4c04e43292cbdfb19362f0462ba709a50a6e54
leekeon215 <leekeon215@gmail.com>
2023-09-15 19:34:24 +0900

    test

--- TimeTable/src/main/MainTable.java
+++ TimeTable/src/main/MainTable.java
@@ -3,7 +3,6 @@ package main;
 public class MainTable {
 	public static void main(String[] args) {
         System.out.println("박수진");
-        
-
+       System.out.println("202211329 이건");
     }
 }

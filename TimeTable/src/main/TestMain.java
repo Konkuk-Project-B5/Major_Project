1cef59c62ffb51154cfa8b852c8f0872fe7172c8
jinssool <jinsol982@konkuk.ac.kr>
2023-10-05 23:36:05 +0900

    이중 HashMap -> Class로 변경

--- TimeTable/src/main/TestMain.java
+++ TimeTable/src/main/TestMain.java
@@ -1,20 +1,13 @@
 package main;
 
-import java.util.HashMap;
-
 public class TestMain {
 
     public static void main(String[] args) {
-        fileReader lecture = new fileReader("lecture_list.txt");
-        lecture.printLectureList();
+        fileReader file = new fileReader("./lecture_list.txt");
+
+        file.printLectureList();
 
 
-        System.out.println();
-
-        HashMap<String, Object> lectureInfo = lecture.getLecture("001");//001을 과목번호로 가짐
-        String value = (String) lectureInfo.get("name"); //과목 정보 추가
-        System.out.println(lectureInfo);
-        System.out.println(value);
 
     }
 

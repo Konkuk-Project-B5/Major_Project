4c54a2766035de22363ced3b00c90d633632b203
leekeon215 <leekeon215@gmail.com>
2023-10-08 06:17:40 +0900

    회원가입 후 학번.txt 생성, 수강신청 과목번호 입력 받고 문법규칙 검사, 수강신청 완료 후 학번.txt에 과목번호 저장

--- TimeTable/src/main/TimeTableManager.java
+++ TimeTable/src/main/TimeTableManager.java
@@ -1,25 +1,32 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TimeTableManager {
 	
 	private static Scanner scan = new Scanner(System.in);
 	private User loginUser; // 로그인한 사용자 정보 저장용 User 객체
+	private BufferedReader reader; // 파일 입력
+	private BufferedWriter writer; // 파일 출력
 		
 	// 생성자
 	public TimeTableManager() {
 		
 		// 파일 무결성 검사 //
 		
-		initSystem(); // 검사 후 프로그램 실행
-	}
-
-	private void initSystem() {
-		loginAndRegisterMenu();	
+		// 검사 후 프로그램 실행, 로그인/회원가입 메뉴 출력
+		loginAndRegisterMenu(); 
 	}
 	
-	// 로그인/회원가입 메뉴
+	// 로그인/회원가입 메뉴 출력 메소드
 	private void loginAndRegisterMenu() {
 		
 		System.out.println("[수강신청 프로그램]");
@@ -78,9 +85,43 @@ public class TimeTableManager {
 		// 로그인 //
 		
 		
+		// 사용자로부터 받은 입력값 검사 후 User 객체 생성
+		String id = "202211329"; // 테스트
+		String password = "a1b2c3"; // 테스트
+		loginUser = new User(id, password);
+		
+		// 학번.txt에서 과목번호 읽고 loginUser의 myLectureList 초기화
+		initUserLectureList();		
+		
 		// 로그인 완료 후 메인 메뉴로 이동
 		System.out.println("로그인을 완료하였습니다.");
 		mainMenu();
+	}
+	
+	// loginUser의 myLectureList 초기화 메소드
+	private void initUserLectureList() {
+		try {
+			reader = new BufferedReader(new FileReader(loginUser.FILEPATH));
+			
+			loginUser.myLectureList = new ArrayList<String>();
+			String lectureNum = null;
+			while((lectureNum = reader.readLine()) != null) {
+				loginUser.myLectureList.add(lectureNum);
+			}
+			
+//			//확인용
+//			for(String l : loginUser.lectureList)
+//				System.out.println(l);
+			
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+//			System.out.println(loginUser.FILEPATH+": 파일 존재하지 않음");
+			System.exit(0); // 오류 발생시 프로그램 종료
+		} catch (IOException e) {
+//			System.out.println(loginUser.FILEPATH+": 읽기 실패");
+			System.exit(0); // 오류 발생시 프로그램 종료
+		}
 	}
 
 	// 회원가입 메소드
@@ -88,10 +129,25 @@ public class TimeTableManager {
 		
 		// 회원 가입 //
 		
+		// 학번.txt 파일 생성
+		String id = "202211329"; // 테스트
+		createIdFile(id);
+		
 		System.out.println("회원가입을 완료했습니다.");
 	}
 	
-	// 수강신청 및 시간표 조회 (메인 메뉴)
+	// 학번.txt 파일 생성 메소드
+	private void createIdFile(String id) {
+		File file = new File("./"+id+".txt");
+		try {
+			file.createNewFile(); 
+		} catch (IOException e) {
+//			System.out.println(id + ": 파일 생성 실패");
+			System.exit(0); // 오류 발생시 프로그램 종료
+		}
+	}
+
+	// 수강신청 및 시간표 조회 (메인 메뉴) 출력 메소드
 	private void mainMenu() {
 		String input = null;
 		
@@ -133,13 +189,61 @@ public class TimeTableManager {
 	
 	// 시간표 조회 메소드
 	private void showTimeTable() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	// 수강신청 메소드
 	private void signUp() {
-		// TODO Auto-generated method stub
+		String input = null;
 		
+		while(true) {
+			
+			// 강의 목록 출력 //
+			
+			System.out.println("\n\n수강신청할 과목번호를 입력해주세요\n※ 'q'를 입력한 경우 메인메뉴로 돌아갑니다.\n\n");
+			System.out.print("과목번호 입력 : ");
+			input = scan.nextLine().strip();
+			
+			if(input.equals("q")) {
+				System.out.println("메인메뉴로 돌아갑니다.");
+				return;
+			}
+			
+			// 문법규칙에 맞는지 검사
+			if(!input.matches("\\d{3}")) {
+				System.out.println("과목번호는 0과 자연수로만 이루어진 길이가 3인 문자열입니다.");
+				continue;
+			}
+			
+			// 중복 시간대 검사, 중복 수강신청, 인원제한 검사 //
+			
+			// 검사 통과시 break
+			break;
+		}
+		
+		// 수강신청한 과목번호 loginUser의 myLectureList에 저장
+		loginUser.myLectureList.add(input);
+										
+		// 학번.txt 파일 업데이트
+		updateIdFile();
+					
+		// 수강신청 완료
+		System.out.println("수강신청이 완료되었습니다.");
+	}
+	
+	// 학번.txt 파일 업데이트 메소드
+	private void updateIdFile() {
+		String content = "";
+		for(String lectureNum : loginUser.myLectureList)
+			content += lectureNum + "\n";
+	
+		try {
+			writer = new BufferedWriter(new FileWriter(loginUser.FILEPATH));
+			writer.write(content);
+			writer.close();
+		} catch (IOException e) {
+//			System.out.println(loginUser.FILEPATH+": 쓰기 실패");
+			System.exit(0); // 오류 발생시 프로그램 종료
+		}
 	}
 }

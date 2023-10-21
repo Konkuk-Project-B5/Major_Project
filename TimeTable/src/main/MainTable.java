3ccd9f014744552d698e38ee19dcef9014860e76
82104 <82104@Airmax>
2023-10-21 23:40:20 +0900

    t

--- TimeTable/src/main/MainTable.java
+++ TimeTable/src/main/MainTable.java
@@ -2,8 +2,7 @@ package main;
 
 public class MainTable {
 	public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        
+        //System.out.println("Hello, world!");
 
     }
 }
